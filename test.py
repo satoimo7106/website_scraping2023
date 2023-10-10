e4b490d8761de69477351ce633e0e24084335069
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2023-10-10 12:30:11 +0900

    Add files via upload
    test2

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+#chatgptに考えてもらった
+#アップロードするときのURLはhttps://example.com/が良さそう
 import requests
 
-url = 'https://scraping-training.vercel.app/site?postCount=30&title=%E3%81%93%E3%82%8C%E3%81%AF{no}%E3%81%AE%E8%A8%98%E4%BA%8B%E3%81%A7%E3%81%99&dateFormat=YYYY-MM-DD&isTime=true&timeFormat=&isImage=true&interval=360&isAgo=true&countPerPage=10&page=1&robots=true&'  # ウェブサイトのURLを指定
+url = 'https://example.com/'  # ウェブサイトのURLを指定
 response = requests.get(url)
 
 if response.status_code == 200:
