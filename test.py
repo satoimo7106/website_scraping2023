38b395e8360e68bb72cf8f3d58f9644f936674d1
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2023-10-13 13:29:38 +0900

    Add files via upload
    ファイル名を変えたほうがいいかも

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 #chatgptに考えてもらった
-#アップロードするときのURLはhttps://example.com/が良さそう
+#アップロードするときのURLはhttps://example.com/が良さそう（実行時はテスト用のサイトに書き換えること）
 import requests
 
-url = 'https://example.com/'  # ウェブサイトのURLを指定
+url = 'http://127.0.0.1:5500/testsite/index.html'  # ウェブサイトのURLを指定
 response = requests.get(url)
 
-#指定したURLにGETリクエストを行う
 if response.status_code == 200:
-    #GETリクエストが成功した場合、HTMLを取得する
     html_content = response.text
 else:
-    #GETリクエストが失敗した場合、取得できないと表示する
     print("ウェブページを取得できませんでした。")
 
 
@@ -27,7 +24,11 @@ for link in links:
     print(link.get('href'))
 
 num_links = len(links)
-num_paragraphs = len(soup.find_all('p'))
 
 print(f"リンク数: {num_links}")
-print(f"段落数: {num_paragraphs}")
+
+
+#スタートのページのリンクを全部取得
+# まだ行ったことがないリンクだったら（繰り返し）
+    # 取得したリンク先に移動
+    # そのページのリンクを全部取得
