0e259ecd91ea37d545d0269e82e111e8780602eb
stoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2023-10-15 00:08:43 +0900

    VSCodeからコミットのテスト

--- test.py
+++ test.py
@@ -1,37 +1,59 @@
-#chatgptに考えてもらった
-#アップロードするときのURLはhttps://example.com/が良さそう（実行時はテスト用のサイトに書き換えること）
 import requests
-
-url = 'http://127.0.0.1:5500/testsite/index.html'  # ウェブサイトのURLを指定
-response = requests.get(url)
-
-if response.status_code == 200:
-    html_content = response.text
-else:
-    print("ウェブページを取得できませんでした。")
-
-
 from bs4 import BeautifulSoup
+import re
 
-soup = BeautifulSoup(html_content, 'html.parser')
+# すべての収集済みURLを格納するセット
+collected_urls = set()
 
-# 例: タイトルを取得
-title = soup.title.string
+# 抽出したテキストを格納するリスト
+extracted_texts = []
 
-# すべてのリンク（<a>要素）を取得
-links = soup.find_all('a')
+# 指定したURLからリンクを収集し、テキストを抽出し、再帰的に処理する関数
+def collect_urls_and_extract_text(url, depth=1, max_depth=3):
+    if depth > max_depth:
+        return
 
+    # URLを収集
+    collected_urls.add(url)
 
-# 各リンクのURLを表示
-for link in links:
-    print(link.get('href'))
+    # ページのコンテンツを取得
+    response = requests.get(url)
+    page_content = response.text
 
-num_links = len(links)
+    # BeautifulSoupを使用してHTMLを解析
+    soup = BeautifulSoup(page_content, 'html.parser')
 
-print("リンク数:"+ str(num_links))
+    # すべてのリンク（<a>要素）を取得
+    links = soup.find_all('a')
 
+    for link in links:
+        href = link.get('href')
+        if href:
+            # 絶対URLに変換
+            if not href.startswith('http'):
+                href = url + href
+            # 収集済みのURLでない場合に再帰的に処理
+            if href not in collected_urls:
+                collect_urls_and_extract_text(href, depth + 1)
 
-#スタートのページのリンクを全部取得
-# まだ行ったことがないリンクだったら（繰り返し）
-    # 取得したリンク先に移動
-    # そのページのリンクを全部取得
+    # テキストを抽出
+    text = soup.get_text()
+    # 正規表現を使用して"test{xxxx}"のパターンを抽出
+    matches = re.findall(r'test\{[^\}]*\}', text)
+    extracted_texts.extend(matches)
+
+# スタートURLを指定
+start_url = 'http://127.0.0.1:5500/testsite/index.html'
+
+# URLを収集し、テキストを抽出
+collect_urls_and_extract_text(start_url)
+
+# 収集済みのURLを表示
+print("Collected URLs:")
+for url in collected_urls:
+    print(url)
+
+# 抽出したテキストを表示
+print("\nExtracted Texts:")
+for text in extracted_texts:
+    print(text)
