f91f06faf03221a008870135bcff9c0763e847fe
stoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2023-10-15 22:30:50 +0900

    修正：コメント、変数名、関数名の修正 改善：キーワード抽出処理を別関数として定義

--- test.py
+++ test.py
@@ -3,18 +3,24 @@ from bs4 import BeautifulSoup
 import re
 
 # すべての収集済みURLを格納するセット
-collected_urls = set()
+url_list = []
 
 # 抽出したテキストを格納するリスト
-extracted_texts = []
+keyword_list = []
 
-# 指定したURLからリンクを収集し、テキストを抽出し、再帰的に処理する関数
-def collect_urls_and_extract_text(url, depth=1, max_depth=3):
-    if depth > max_depth:
-        return
+#キーワードを抽出する処理
+def get_keyword(text):
+    # 正規表現を使用して"TEST{xxxx}"のパターンを抽出
+    matches = re.findall(r'TEST\{[^\}]*\}', text)
+    keyword_list.extend(matches)
+    return
+
+# 指定したURLからリンクを収集、再帰的に処理する関数
+#関数名を後で変えたい
+def get_urls(url):
 
     # URLを収集
-    collected_urls.add(url)
+    url_list.append(url)
 
     # ページのコンテンツを取得
     response = requests.get(url)
@@ -26,34 +32,36 @@ def collect_urls_and_extract_text(url, depth=1, max_depth=3):
     # すべてのリンク（<a>要素）を取得
     links = soup.find_all('a')
 
+    # テキストを抽出
+    text = soup.get_text()
+
+    #【要修正】（再帰的に、そのページのすべてのキーワードを収集するようにする）
+    #keywordを抽出する関数
+    get_keyword(text)
+
     for link in links:
+        #aタグのhref属性（URL部分）を取得
         href = link.get('href')
         if href:
-            # 絶対URLに変換
+            # 【要修正】絶対URLに変換（URLが"http"から始まっていない場合）
             if not href.startswith('http'):
                 href = url + href
             # 収集済みのURLでない場合に再帰的に処理
-            if href not in collected_urls:
-                collect_urls_and_extract_text(href, depth + 1)
+            if href not in url_list:
+                get_urls(href)
 
-    # テキストを抽出
-    text = soup.get_text()
-    # 正規表現を使用して"TEST{xxxx}"のパターンを抽出
-    matches = re.findall(r'TEST\{[^\}]*\}', text)
-    extracted_texts.extend(matches)
-
-# スタートURLを指定
+#【要修正】スタートURLを指定（将来的には利用者が入力できるように）
 start_url = 'http://127.0.0.1:5500/testsite/index.html'
 
-# URLを収集し、テキストを抽出
-collect_urls_and_extract_text(start_url)
+# URLを収集
+get_urls(start_url)
 
 # 収集済みのURLを表示
-print("Collected URLs:")
-for url in collected_urls:
+print("URLs:")
+for url in url_list:
     print(url)
 
 # 抽出したテキストを表示
-print("\nExtracted Texts:")
-for text in extracted_texts:
-    print(text)
+print("\nKeyWord:")
+for keyword in keyword_list:
+    print(keyword)
