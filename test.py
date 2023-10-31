0192b51ec67e83d5fdbf009646a87dfff3d41df7
satoimo7106 <97589420+satoimo7106@users.noreply.github.com>
2023-10-31 13:10:09 +0900

    作りかけ

--- test.py
+++ test.py
@@ -6,96 +6,140 @@ import urllib.parse
 #とりあえずのエラー処理
 try:
 
-    # URLを格納するリスト
-    url_list = []
+    def main():
+        # URLを格納するリスト
+        url_list = []
 
-    #パラメータを格納するリスト
-    parameter_list=[]
+        #パラメータを格納するリスト
+        parameter_list=[]
 
-    #ページタイトルを格納するリスト
-    title_list=[]
+        #ページタイトルを格納するリスト
+        title_list=[]
 
-    # 抽出したキーワード(MBSD{xxxx})を格納するリスト
-    keyword_list = []
+        # 抽出したキーワード(MBSD{xxxx})を格納するリスト
+        keyword_list = []
 
-    #スタートURLを指定（将来的には利用者が入力できるように）
-    print("対象サイトのURLを入力してください（例：https://example.com/)")
-    start_url=input() #入力する場合（CUI)
+        # #スタートURLを指定（将来的には利用者が入力できるように）
+        # print("対象サイトのURLを入力してください（例：https://example.com/)")
+        # start_url=input() #入力する場合（CUI)
 
-    #対象のドメインを指定
-    print("対象のドメインを入力してください（例：example.com)")
-    target_domain=input() #入力する場合（CUI)
+        # #対象のドメインを指定
+        # print("対象のドメインを入力してください（例：example.com)")
+        # target_domain=input() #入力する場合（CUI)
 
-    #URLからリンクを収集、再帰的に処理する関数
-    def get_urls(url):
+        #URLからリンクを収集、再帰的に処理する関数
+        def get_urls(url):
 
-        # URLをurl_listに追加
-        url_list.append(url)
+            # URLをurl_listに追加
+            url_list.append(url)
+            
+            # ページのコンテンツを取得
+            response = requests.get(url)
+
+            # BeautifulSoupを使用してHTMLを解析
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # すべてのリンク（<a>要素）を取得
+            links = soup.find_all('a')
+
+            # テキストを抽出
+            website_text = soup.get_text()
+
+            #URLからパラメータ（q="xxxx")を取得
+            parameter=urllib.parse.urlparse(url).query
+            #パラメータが取得できた場合、パラメータリストに追加
+            if parameter:
+                parameter_list.append(parameter)
+            #【要検討】取得できなかった場合、パラメータリストに"なし"を追加
+            else:
+                parameter_list.append("なし")
+
+            #ページタイトルを取得
+            #ページタイトルが取得できた場合、ページタイトルリストに追加
+            if soup.title:
+                title_list.append(soup.title.string)
+            #取得できなかった場合、ページタイトルリストに"なし"を追加
+            else:
+                title_list.append("なし")
+
+
+            #キーワードを抽出する
+            # 正規表現を使用して"MBSD{xxxx}"を抽出して一致したものをすべてリストに格納
+            keyword=re.findall(r'MBSD\{[^\}]*\}', website_text)
+
+            for k in keyword:
+                #キーワードリストに含まれていない場合に追加
+                if k not in keyword_list:
+                    keyword_list.append(k)
+
+
+            for link in links:
+                #aタグのhref属性（URL部分）を取得
+                href = link.get('href')
+                if href and href not in url_list and target_domain in href:
+                    # 収集済みのURLでない場合に再帰的に処理
+                    get_urls(href)
+
+        #URLを収集
+        if target_domain in start_url:
+            get_urls(start_url)
+
+        #収集済みのURLを表示
+        print("\nURL|パラメータ|ページタイトル")
+        for url,parameter,title in zip(url_list,parameter_list,title_list):
+            print(url,parameter,title)
+
+        #キーワードリストを表示
+        print("\nKeyWord:")
+        for k in keyword_list:
+            print(k)
         
-        # ページのコンテンツを取得
-        response = requests.get(url)
-
-        # BeautifulSoupを使用してHTMLを解析
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # すべてのリンク（<a>要素）を取得
-        links = soup.find_all('a')
-
-        # テキストを抽出
-        website_text = soup.get_text()
-
-        #URLからパラメータ（q="xxxx")を取得
-        parameter=urllib.parse.urlparse(url).query
-        #パラメータが取得できた場合、パラメータリストに追加
-        if parameter:
-            parameter_list.append(parameter)
-        #【要検討】取得できなかった場合、パラメータリストに"なし"を追加
-        else:
-            parameter_list.append("なし")
-
-        #ページタイトルを取得
-        #ページタイトルが取得できた場合、ページタイトルリストに追加
-        if soup.title:
-            title_list.append(soup.title.string)
-        #取得できなかった場合、ページタイトルリストに"なし"を追加
-        else:
-            title_list.append("なし")
-
-
-        #キーワードを抽出する
-        # 正規表現を使用して"MBSD{xxxx}"を抽出して一致したものをすべてリストに格納
-        keyword=re.findall(r'MBSD\{[^\}]*\}', website_text)
-
-        for k in keyword:
-            #キーワードリストに含まれていない場合に追加
-            if k not in keyword_list:
-                keyword_list.append(k)
-
-
-        for link in links:
-            #aタグのhref属性（URL部分）を取得
-            href = link.get('href')
-            if href and href not in url_list and target_domain in href:
-                # 収集済みのURLでない場合に再帰的に処理
-                get_urls(href)
-
-    #URLを収集
-    if target_domain in start_url:
-        get_urls(start_url)
-
-    #収集済みのURLを表示
-    print("\nURL|パラメータ|ページタイトル")
-    for url,parameter,title in zip(url_list,parameter_list,title_list):
-        print(url,parameter,title)
-
-    #キーワードリストを表示
-    print("\nKeyWord:")
-    for k in keyword_list:
-        print(k)
-    
-    input("Enterを押すと終了します")
+        input("Enterを押すと終了します")
         
 #何かしらのエラーが発生したらエラー表示
 except Exception: 
     print("エラーが発生しました")
     input("Enterを押すと終了します")
+
+import tkinter as tk
+
+# ボタンがクリックされたときの処理
+def calculate():
+    try:
+        # 入力欄から数値を取得
+        num1 = float()
+        num2 = float(entry2.get())
+        
+        # 数値を合計し、結果をテキストフィールドに表示
+        result.set(f"合計: {num1 + num2}")
+    except ValueError:
+        result.set("エラー: 有効な数値を入力してください")
+
+# Tkinterウィンドウの作成
+window = tk.Tk()
+window.title("ここにタイトル")
+
+# ラベルと入力欄の作成
+label1 = tk.Label(window, text="URL:")
+label1.pack()
+
+entry1 = tk.Entry(window)
+entry1.pack()
+
+label2 = tk.Label(window, text="ドメイン:")
+label2.pack()
+
+entry2 = tk.Entry(window)
+entry2.pack()
+
+# ボタンの作成
+calculate_button = tk.Button(window, text="出力", command=lambda:get_urls(str(entry1.get())))
+calculate_button.pack()
+
+# 結果を表示するためのテキストフィールド
+result = tk.StringVar()
+result_label = tk.Label(window, textvariable=result)
+result_label.pack()
+
+# ウィンドウのメインループ
+window.mainloop()
